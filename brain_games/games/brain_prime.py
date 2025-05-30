7b88c3809a96db3507ceea04187c3eb402e16ee0
lkimka <fekla7@bk.ru>
2025-05-30 12:38:21 +0300

    brain-prime

---
+++ brain_games/games/brain_prime.py
@@ -0,0 +1,24 @@
+from random import randint
+
+from brain_games.scripts.engine import start_game
+
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def question_and_answer():
+    number = randint(1, 100)
+    question = str(number)
+    correct_answer = "yes" if is_prime(number) else "no"
+    return question, correct_answer
+
+
+def main():
+    game_rule = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+    start_game(question_and_answer, game_rule, "Prime Game")
